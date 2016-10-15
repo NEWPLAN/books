9646072454cc16d879070fdcd8b95a8eea26ebc3
NEWPLAN <enwplan001@163.com>
2016-10-15 22:50:49 +0800

    fix error

--- oranges/chapter1/a/boot.asm
+++ oranges/chapter1/a/boot.asm
@@ -7,12 +7,13 @@
 DispStr:
 	mov	ax, BootMessage
 	mov	bp, ax			; ES:BP = 串地址
-	mov	cx, 16			; CX = 串长度
+	mov	cx, msgLen		; CX = 串长度
 	mov	ax, 01301h		; AH = 13,  AL = 01h
 	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
 	int	10h			; 10h 号中断
 	ret
 BootMessage:		db	"Hello, OS world!"
+msgLen:	equ $-BootMessage; get the length of string
 times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw 	0xaa55				; 结束标志
