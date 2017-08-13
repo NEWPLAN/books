c878a26e10317e39e5976bf1fb28817067fe8611
NEWPLAN <enwplan001.com>
2017-08-13 19:57:38 +0800

    update

---
+++ programming/chap6/abc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <sdlib.h>
+
+int main(int argc, char** argv)
+{
+    if(1)
+        printf("hello world!");
+    if(2)
+    {
+        printf("hello world!");
+        return 0;
+    }
+	printf("hello, world!");
+	return 0;
+}
---
+++ programming/chap6/second.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: second.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年08月02日 星期三 20时06分49秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc,char** argv)
+{
+    int a=10;
+    if(0)
+    {
+        printf("eee");
+    }
+    printf("hello world!\n");
+    return 0;
+}
