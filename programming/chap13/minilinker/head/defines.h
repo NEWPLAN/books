f3e2ed3b599a1d7ce7e60f6b31cb97e904aadd77
Yang Cheng <enwplan001@163.com>
2017-08-16 16:02:06 +0800

    add a linker

---
+++ programming/chap13/minilinker/head/defines.h
@@ -0,0 +1,17 @@
+/*
+ * defines.h
+ *
+ *  Created on: Jul 27, 2013
+ *      Author: ns
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+//#define DEBUG
+
+typedef unsigned char u8;
+const int ENTRY_ADDRESS = 0x400000;
+const int PAGE_SIZE_ALGIN = 12;		//4KB = 2^12
+
+#endif /* DEFINES_H_ */
---
+++ programming/chap13/minilinker/readme.md
@@ -0,0 +1,13 @@
+出于学习目的写的一个C链接器。
+
+相关博文:
+
+- [一个链接器的实现「一」](http://www.cnblogs.com/nanshu/p/3231645.html)
+- [一个连接器的实现「二」](http://www.cnblogs.com/nanshu/p/3240156.html)
+
+使用：
+```
+-o 指定输出路径
+-l 指定minicrt库的路径(目录下应该全都是.o文件)
+用户的.o文件
+```
